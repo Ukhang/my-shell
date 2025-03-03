7f232b99ee491c6c6a3c2e4349a1dccaa4ffe168
Ukhang <ukhangmarma459@gmail.com>
2025-03-03 15:39:28 +0600

    implement Pwd command extending BuiltinCommand

---
+++ src/commands/Pwd.ts
@@ -0,0 +1,8 @@
+import { BuiltinCommand } from "./BuiltinCommand";
+import path from "path";
+
+export class Pwd extends BuiltinCommand {
+    execute(): void {
+        console.log(path.resolve(process.cwd()));
+    }
+}
