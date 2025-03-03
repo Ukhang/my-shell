d2e16861bd231a6e9ff8fb9ffe87c3b581b96c26
Ukhang <ukhangmarma459@gmail.com>
2025-03-03 16:25:32 +0600

    implement Touch command for file creation

--- src/Executor.ts
+++ src/Executor.ts
@@ -4,6 +4,7 @@ import { Exit } from "./commands/Exit";
 import { Pwd } from "./commands/Pwd";
 import { Cd } from "./commands/Cd";
 import { Ls } from "./commands/Ls";
+import { Touch } from "./commands/Touch";
 
 export class Executor {
   private builtins: { [key: string]: BuiltinCommand } = {};
@@ -14,6 +15,7 @@ export class Executor {
     this.builtins["pwd"] = new Pwd();
     this.builtins["cd"] = new Cd();
     this.builtins["ls"] = new Ls();
+    this.builtins["touch"] = new Touch();
   }
 
   async execute(command: string): Promise<void> {
---
+++ src/commands/Touch.ts
@@ -0,0 +1,23 @@
+import { BuiltinCommand } from "./BuiltinCommand";
+import * as fs from "fs";
+
+export class Touch extends BuiltinCommand {
+  execute(args: string[]) {
+    const filename = args[0];
+    if (!filename) {
+      console.log("Usage: touch <filename>");
+      return;
+    }
+
+    try {
+      fs.writeFileSync(filename, "", { flag: "wx" });
+      console.log(`File ${filename} created`);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error("Error creating file:", err.message);
+      } else {
+        console.error("Unknown error occurred while creating the file.");
+      }
+    }
+  }
+}
