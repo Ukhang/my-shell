c9702674daa52a7fca0505c21779225af1b09791
Ukhang <ukhangmarma459@gmail.com>
2025-03-03 15:46:58 +0600

    add multiple builtin commands (echo, exit, pwd, cd) to Executor

--- src/Executor.ts
+++ src/Executor.ts
@@ -1,11 +1,17 @@
 import { BuiltinCommand } from "./commands/BuiltinCommand";
+import { Echo } from "./commands/Echo";
 import { Exit } from "./commands/Exit";
+import { Pwd } from "./commands/Pwd";
+import { Cd } from "./commands/Cd";
 
 export class Executor {
   private builtins: { [key: string]: BuiltinCommand } = {};
 
   constructor() {
     this.builtins["exit"] = new Exit();
+    this.builtins["echo"] = new Echo();
+    this.builtins["pwd"] = new Pwd();
+    this.builtins["cd"] = new Cd();
   }
 
   execute(command: string) {
