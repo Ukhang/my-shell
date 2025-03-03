77c0f9d9d50ea4ebd7715a54706fd371ad63cd71
Ukhang <ukhangmarma459@gmail.com>
2025-03-03 15:09:46 +0600

    implement Exit command

---
+++ src/commands/Exit.ts
@@ -0,0 +1,8 @@
+import { BuiltinCommand } from "./BuiltinCommand";
+
+export class Exit extends BuiltinCommand {
+    execute(): void {
+        console.log("Exiting shell");
+        process.exit(0);
+    }
+}
